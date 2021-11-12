a0cb486d73da767dad2f8dbb1d410cf7c7e27505
Tina <me@tina.moe>
2021-11-12 21:59:04 +0100

    Helper parameters for generic reply method

--- mechadon/cogs/__init__.py
+++ mechadon/cogs/__init__.py
@@ -3,16 +3,39 @@ from discord.ext.commands import Cog, Context
 from discord.mentions import AllowedMentions
 
 class BaseCog(Cog):
+    MAX_MESSAGE_LENGTH = 4096
+
     def __init__(self, bot: Bot = None):
         self.bot = bot
 
-    async def reply(self, context: Context, *words, sep=' ', sort=False):
-        words = list(map(str, words))
+    async def reply(
+        self,
+        context: Context,
+        *words,
+        list_=None,
+        formatter=str,
+        sep=' ',
+        sort=False,
+        total=False
+    ):
+        if list_:
+            words = list_
+        words = list(map(formatter, words))
         if sort:
             words.sort(key=lambda x: x.lower())
         msg = sep.join(words) or '*Empty*'
+        if total:
+            msg = f'Total: {len(words)}\n\n' + msg
+        if len(msg) > self.MAX_MESSAGE_LENGTH:
+            msg = msg[:self.MAX_MESSAGE_LENGTH - 2] + '\n…'
         embed = Embed(description=msg)
         return await context.reply(embed=embed, allowed_mentions=AllowedMentions.none())
+
+    async def reply_list(self, context: Context, list_, **kwargs):
+        kwargs.setdefault('sep', '\n')
+        kwargs.setdefault('sort', True)
+        kwargs.setdefault('total', True)
+        return await self.reply(context, *list_, **kwargs)
 
     @staticmethod
     def get_roles_by_id(context):
