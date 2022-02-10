e46c035c7ed5d96e84c290778f5124d09f88f35c
Tina <me@tina.moe>
2022-02-10 23:33:25 +0100

    Update ping cog

--- mechadon/cogs/ping.py
+++ mechadon/cogs/ping.py
@@ -1,7 +1,15 @@
+from datetime import datetime
+
 from . import BaseCog, Context, commands
 
 
 class PingCog(BaseCog):
     @commands.command()
     async def ping(self, context: Context):
-        await context.reply('pong!')
+        delta = datetime.utcnow().second - context.message.created_at.second
+        await context.reply(
+            '```'
+            '\nPING Discord 56(84) bytes of data.'
+            f'\n64 bytes from Discord: icmp_seq=1 ttl=56 time={delta / 1000}'
+            '```'
+        )
