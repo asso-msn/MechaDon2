a9d0ec4a3aefb288866854b82eb219c5bd1ce242
Tina <me@tina.moe>
2022-10-08 20:05:11 +0200

    genesis gacha: Use both salt and ID

--- mechadon/cogs/gacha.py
+++ mechadon/cogs/gacha.py
@@ -12,5 +12,6 @@ class GachaCog(BaseCog):
     @gacha.command()
     async def genesis(self, context):
         url = "https://fairyjoke.net/sdvx/genesis/gacha/card.png"
-        url += "?id=" + random.randint(0, 1000000000)
+        url += "?salt=" + secrets.token_urlsafe(16)
+        url += "&id=" + random.randint(0, 1000000000)
         await context.send(url)
