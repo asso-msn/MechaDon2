8abe454cc8fd3294dce42d92bed8f526f6f75c71
Tina <skielred@gmail.com>
2024-07-10 11:34:10 +0200

    Rewrite user formatter to exclude more cases of similar names and special chars

--- mechadon/formatters.py
+++ mechadon/formatters.py
@@ -2,6 +2,29 @@ import discord
 
 
 def member(member: discord.Member):
-    if member.nick:
-        return f"{member.nick} aka {member._user}"
-    return str(member._user)
+    def similar(*args):
+        args = [x.lower() for x in args if x]
+        if len(set(args)) == 1:
+            return args[0]
+        return False
+
+    result = member.nick
+    if result:
+        if not similar(result, member.global_name):
+            result += f" aka {member.global_name}"
+    else:
+        result = member.global_name
+
+    if result:
+        if not (
+            (member.nick and similar(member.nick, member.name))
+            or similar(member.global_name, member.name)
+        ):
+            result += f" (@{member.name})"
+    else:
+        result = member.name
+
+    for c in ("_", "*", "`", "~", "|", ">", "#"):
+        result = result.replace(c, f"\\{c}")
+
+    return result
